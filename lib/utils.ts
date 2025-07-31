31f31d38f7b3cb5c1cea6e280a2c93182440346d
laurentvuillaume <figura.graphik@gmail.com>
2025-07-31 13:01:14 +0200

    update utils file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// Con vert prisma object to regular object
+export function convertToPlainObject<T>(value: T): T {
+  console.log("value: ", value);
+  return JSON.parse(JSON.stringify(value));
+}
+
+// Format number width decimal plaaces
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
