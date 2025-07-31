4bacd4e80a06a8e28260c1b486d8d05b84c8ee76
laurentvuillaume <figura.graphik@gmail.com>
2025-07-31 12:57:09 +0200

    update product page

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
-
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import ProductPrice from "@/components/shared/products/product-price";
+import ProductImages from "@/components/shared/products/product-images";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 
 
@@ -11,10 +16,77 @@ const ProductPage = async ({ params }: { params: Promise<{ slug: string }> }) =>
   if (!product) notFound
 
   return (
-    <div className="my-10">
-      <h1 className="text-2xl font-bold mb-4">{product?.name}</h1>
-     
-    </div>
+    <section>
+        <div className="grid grid-cols-1 md:grid-cols-5">
+            {/* Image Column */}
+            <div className="col-span-2">
+                <ProductImages images={product?.images} />
+            </div>
+            {/* Details Column */}
+            <div className="col-span-2 p-5">
+                <div className="flex flex-col gap-6">
+                    <p>
+                        {product?.brand} {product?.category}
+                    </p>
+                    <h1 className="h3-bold">{product?.name}</h1>
+                    <p>
+                        {product?.rating} of {product?.numReviews} Reviews
+                    </p>
+                    <div className="flex flex-col gap-3 sm:flex-row sm:items-center gap-3">
+                        <ProductPrice 
+                            price={Number(product?.price)}
+                            className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+                        />
+                    </div>
+                </div>
+                <div className="mt-10">
+                    <p className="font-semibold">
+                        Description
+                    </p>
+                        <p>{product?.description}  </p>
+                    
+                </div>
+            </div>
+            
+            {/* Action Column */}
+            <div className="col-span-1 p-5">
+                <Card>
+                    <CardContent className="p-4">
+                        <div className="mb-2 flex justify-between">
+                            <div>Price</div>
+                            <div>
+                                <ProductPrice
+                                    price={Number(product?.price)}
+                                    
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-2 flex justify-between">
+                            <div>Status</div>
+                            {product?.stock > 0 ? (
+                                <Badge variant='outline'>
+                                    In Stock
+                                </Badge>
+                            ) : (
+                                <Badge variant='destructive'>
+                                    Out of Stock
+                                </Badge>
+                            )}
+                           
+                        </div>
+                        {product?.stock > 0 && (
+                            <div className="flex-center">
+                                <Button className="w-full">
+                                    Add to Cart
+                                </Button>
+                            </div>
+                        )}
+                    </CardContent>
+                </Card>
+            </div>
+            
+        </div>
+    </section>
   );
 };
 
