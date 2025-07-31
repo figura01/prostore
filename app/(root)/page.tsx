b5cd058fa95c6d2093768dc248bc387d5d910873
laurentvuillaume <figura.graphik@gmail.com>
2025-07-31 12:56:11 +0200

    update (root) page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
+import ProductList from "@/components/shared/products/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+import { convertToPlainObject } from "@/lib/utils";
 
+const HomePage = async () => {
+  // Fetch latest products
+  const latestProducts = await getLatestProducts();
 
-export default function Home() {
   return (
-    <>Prostore</>
+    <>
+      <ProductList 
+        data={latestProducts} 
+        title="Newest Arrivals"
+        limit={6}
+      />
+    </>
   );
-}
+};
+
+export default HomePage;
+
