e2799ad0d6b576cd53b1fa0d6e9cc5510dd9c4d6
laurentvuillaume <figura.graphik@gmail.com>
2025-08-03 15:41:52 +0200

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const token = request.cookies.get("auth_token");
-  // Check if the user is on a protected page and is not authenticated
-  if (pathname.startsWith("/protected") && !token) {
-    return Response.redirect(new URL("/login", request.url));
-  }
-  // Add a custom header to the response
-  const response = NextResponse.next();
-  response.headers.set("X-Custom-Header", "This is a custom header");
-  return response;
-}
+export const { auth: middleware } = NextAuth(authConfig);
