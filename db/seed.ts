7bb51a1e8a2a7e5a0ae2e6ee1da2c6d6a464734a
laurentvuillaume <figura.graphik@gmail.com>
2025-08-03 15:39:25 +0200

    update db/seed

--- db/seed.ts
+++ db/seed.ts
@@ -5,9 +5,14 @@ async function main() {
   const prisma = new PrismaClient();
   try {
     await prisma.product.deleteMany(); // Clear existing products
-    await prisma.product.createMany({
-      data: sampleData.products,
-    });
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.user.deleteMany();
+
+    await prisma.product.createMany({ data: sampleData.products });
+    await prisma.user.createMany({ data: sampleData.users });
+
     console.log("Sample data seeded successfully.");
   } catch (error) {
     console.error("Error seeding data:", error);
