2aa8e1e0c389f391f7f44ff81011bbfde3dc4492
laurentvuillaume <figura.graphik@gmail.com>
2025-08-04 21:09:15 +0200

    update user.actions for logout

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,7 +9,6 @@ export async function signInWithCredentials(
   prevState: unknown,
   formData: FormData
 ) {
-  console.log("formData: ", formData);
   try {
     const user = signInFormSchema.parse({
       email: formData.get("email"),
