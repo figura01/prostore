10ecd924b7bd510d34f297bbe2c282000f28229e
laurentvuillaume <figura.graphik@gmail.com>
2025-08-15 00:14:26 +0200

    add plce order page, and implement types & validators for Order and OrderItem

--- lib/validators.ts
+++ lib/validators.ts
@@ -87,3 +87,26 @@ export const paymentMethodSchema = z
     path: ["type"],
     message: "Invalid payment method",
   });
+
+// Schema to inserting order
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, "User is required"),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+    message: "Invvalid payment",
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+// Schema for inserting an order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  name: z.string(),
+  price: z.string(),
+  qty: z.number(),
+});
