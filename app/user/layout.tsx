026ee41854b6aa255a34c3804549310c658ae5ec
laurentvuillaume <figura.graphik@gmail.com>
2025-08-25 16:23:30 +0200

    add pagination for user/orders and create pagination component

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -30,7 +30,7 @@ export default function UserLayout({
                     </div>
                 </div>
             </div>
-            <div className="flex-1 space-y-4 p-8 pt-6 containera mx-auto">
+            <div className="flex-1 space-y-4 p-8 pt-6 container mx-auto">
                 {children}
             </div>
         </div>
---
+++ components/shared/pagination.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { formUrlQuery } from "@/lib/utils";
+
+type PaginationProps = {
+    page: number | string;
+    totalPages: number;
+    urlParamName?: string; // default to 'page'
+}
+
+const Pagination = ({ page, totalPages, urlParamName} : PaginationProps) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const handleClick = (btnType: string) => {
+        const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1;
+        console.log("page: ", page);
+        const newUrl = formUrlQuery({
+            params: searchParams.toString(),
+            key: urlParamName || 'page',
+            value: pageValue.toString()
+        });
+        
+        if(newUrl) router.push(newUrl);
+       
+    }
+
+    return (
+        <div className="flex gap-2">
+            <Button 
+                size="lg" 
+                variant="outline" 
+                className="w-28" disabled={Number(page) <= 1}
+                onClick={() => handleClick('previus')}
+            >
+                Previus
+            </Button>
+            <Button 
+                size="lg" 
+                variant="outline" 
+                className="w-28" 
+                disabled={Number(page) >= totalPages}
+                onClick={() => handleClick('next')}
+            >
+                Next
+            </Button>
+        </div>
+
+    );
+}
+ 
+export default Pagination;
