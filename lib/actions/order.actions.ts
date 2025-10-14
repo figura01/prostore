0a4511a82ea708c914cb5d8237fdc4612dc95f84
laurentvuillaume <figura.graphik@gmail.com>
2025-10-14 10:19:22 +0200

    add confirmed payment by mail

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,12 +7,13 @@ import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 import { prisma } from "@/db/prisma";
-import { CartItem, PaymentResult } from "@/types";
+import { CartItem, PaymentResult, ShippingAddress } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PAGE_SIZE } from "../constants";
 import { Prisma } from "@prisma/client";
 import { _success } from "zod/v4/core";
+import { sendPurchaseReceipt } from "@/email";
 
 // Create order and the order items
 export async function createOrder() {
@@ -263,6 +264,14 @@ export async function updateOrderToPaid({
   });
 
   if (!updatedOrder) throw new Error("Order not found after update");
+
+  sendPurchaseReceipt({
+    order: {
+      ...updatedOrder,
+      shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+      paymentResult: updatedOrder.paymentResult as PaymentResult,
+    },
+  });
 }
 
 // Get user's orders
